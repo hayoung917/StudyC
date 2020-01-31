a446d73e77c1ff48f0335a8a21d9d7e2d3afd7c7
hayoung917 <gkdudp9@naver.com>
2020-01-31 11:11:16 +0900

    2장 작업

--- TemplateTest/main.c
+++ TemplateTest/main.c
@@ -8,8 +8,14 @@
 int main()
 {
 	printf("Hello world!\n");
+	printf("Hi second again\n");
 
 	// type here
+	printf("Be happy\n");
+	printf("1234567890\n");
+	printf("My\tfriend\n");
+	printf("Goot\bd\tchance\n");
+	printf("Cow\rW\a\n");
 
 	system("pause");
 	return 0;
