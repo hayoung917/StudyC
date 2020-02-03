d3d8116a652e9bc220642752b3d4ee7c233532ca
hayoung917 <gkdudp9@naver.com>
2020-02-03 10:38:58 +0900

    4장 완료

--- OpTestApp/main.c
+++ OpTestApp/main.c
@@ -13,8 +13,24 @@
 // 메인함수
 int main(void) 
 {
-	printf("Hello World!\n");
-    // type here.
+    ////elseif 대신에 사용가능
+    //int a = 10, b = 20;
+    //int res = 0;
+
+    //res = (a > b) ? a : b;
+
+    //printf("res = %d\n", res);
+
+    int a = 10, b = 20;
+
+    //비트연산자
+    printf("a & b : %d\n", a & b);
+    printf("a ^ b : %d\n", a ^ b);
+    printf("a | b : %d\n", a | b);
+    printf("~a : %d\n", ~a);
+    printf("a >> 1 : %d\n", a >> 1);
+    printf("a << 2 : %d\n", a << 2);
+
 	system("pause");
 	return EXIT_SUCCESS;
 }
