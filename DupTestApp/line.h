ae0c0ffc430aa19401230d27e179d395c179f93b
hayoung917 <gkdudp9@naver.com>
2020-02-11 12:16:43 +0900

    19장 완료

---
+++ DupTestApp/line.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "point.h"
+
+#ifndef _LINE_H_ //중복제거
+#define _LINE_H_
+
+typedef struct
+{
+	Point first;
+	Point second;
+}Line;
+
+#endif //!_LINE_H_
---
+++ DupTestApp/point.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#ifndef _POINT_H_ //중복제거
+#define _POINT_H_
+
+typedef struct
+{
+	int x;
+	int y;
+}Point;
+
+#endif // !_POINT_H_
+
+
